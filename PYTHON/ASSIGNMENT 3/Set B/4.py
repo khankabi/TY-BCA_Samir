9d9606afb852b8e8c11c69e83647cb9e365c4817
Kabir <115586435+khankabi@users.noreply.github.com>
2023-10-31 22:12:29 +0530

    ass 3

---
+++ PYTHON/ASSIGNMENT 3/Set B/4.py
@@ -0,0 +1,9 @@
+# Write a Python program to get the 4th element from front and 4th element from last of a tuple.
+t = ("w", 3, "r", "e", "s", "o", "u", "r", "c", "e")
+print(t)
+#Get item (4th element)of the tuple by index
+i = t[3]
+print("4th Elements From Tuple :",i)
+#Get item (4th element from last)by index negative
+j = t[-4]
+print("4th Elements From Last Tuple :",j)
